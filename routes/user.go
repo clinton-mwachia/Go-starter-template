866c7b7c371d11c929174f20c9b58a615375248e
clinton moshe mwachia <clintonmwachia9@gmail.com>
2023-11-23 16:02:49 +0300

    added a function to add new user

--- routes/user.go
+++ routes/user.go
@@ -1,10 +1,14 @@
 package routes
 
 import (
+	"Go-starter-template/models"
 	"context"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UsersHandler struct {
@@ -19,8 +23,28 @@ func NewUsersHandler(ctx context.Context, collection *mongo.Collection) *UsersHa
 	}
 }
 
-func (handler *UsersHandler) GetUsers(c *gin.Context) {
+func (handler *UsersHandler) AddNewUser(c *gin.Context) {
+	var user models.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		return
+	}
+	user.Password = string(hashedPassword)
+	user.ID = primitive.NewObjectID()
+	result, err := handler.collection.InsertOne(handler.ctx, user)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		return
+	}
+
 	c.JSON(200, gin.H{
-		"message": "Hello from users",
+		"message": "User Created",
+		"ID":      result.InsertedID,
 	})
 }
