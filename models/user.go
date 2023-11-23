f7f77e4e06085b5cbd3f1ecb8610099e890a517a
clinton moshe mwachia <clintonmwachia9@gmail.com>
2023-11-23 16:24:27 +0300

    added user validation

--- models/user.go
+++ models/user.go
@@ -3,8 +3,8 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
-	ID       primitive.ObjectID `json:"id,omitempty" bson:"id,omitempty"`
-	Password string             `json:"password" bson:"password" validate:"required"`
-	Username string             `json:"username" bson:"username" validate:"required"`
-	Role     string             `json:"role" bson:"role" default:"user" validate:"required,oneof=superadmin admin user"`
+	ID       primitive.ObjectID `json:"id" bson:"id" binding:"omitempty"`
+	Password string             `json:"password" bson:"password" binding:"required"`
+	Username string             `json:"username" bson:"username" binding:"required"`
+	Role     string             `json:"role" bson:"role" default:"user" binding:"required,oneof=superadmin admin user"`
 }
