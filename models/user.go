195ded1d1dd52aa6af69ef45d055c25005429e45
clinton moshe mwachia <clintonmwachia9@gmail.com>
2023-11-27 11:13:42 +0300

    removed required binding

--- models/user.go
+++ models/user.go
@@ -4,7 +4,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
 	ID       primitive.ObjectID `json:"id" bson:"id" binding:"omitempty"`
-	Password string             `json:"password" bson:"password" binding:"required"`
-	Username string             `json:"username" bson:"username" binding:"required"`
-	Role     string             `json:"role" bson:"role" default:"user" binding:"required,oneof=superadmin admin user"`
+	Password string             `json:"password" bson:"password"`
+	Username string             `json:"username" bson:"username"`
+	Role     string             `json:"role" bson:"role" binding:"oneof=superadmin admin user"`
 }
