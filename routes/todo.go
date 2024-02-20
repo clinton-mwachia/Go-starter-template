a650f91b245a3d6da343570e7bf309c960ce3dd9
clinton moshe mwachia <clintonmwachia9@gmail.com>
2024-02-20 18:21:00 +0300

    added a function to get todos with pagination

--- routes/todo.go
+++ routes/todo.go
@@ -3,9 +3,11 @@ package routes
 import (
 	"Go-starter-template/models"
 	"context"
+	"math"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
@@ -234,4 +236,59 @@ func (handler *TodosHandler) CountTodosByPriorityHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"TotalTodos": count,
 	})
+}
+
+/* get todo with pagination */
+func (handler *TodosHandler) ListTodosWithPagHandler(c *gin.Context) {
+	// Extract query parameters for pagination
+	page, err := strconv.Atoi(c.DefaultQuery("page", "1"))
+	if err != nil || page < 1 {
+		page = 1 // Default to page 1 if invalid or not provided
+	}
+
+	pageSize, err := strconv.Atoi(c.DefaultQuery("pageSize", "10"))
+	if err != nil || pageSize < 1 {
+		pageSize = 10 // Default to 10 items per page if invalid or not provided
+	}
+
+	// Calculate skip value for pagination
+	skip := (page - 1) * pageSize
+
+	// MongoDB find options for pagination
+	findOptions := options.Find()
+	findOptions.SetSkip(int64(skip))
+	findOptions.SetLimit(int64(pageSize))
+
+	// Perform find operation with pagination options
+	cur, err := handler.collection.Find(handler.ctx, bson.M{}, findOptions)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer cur.Close(handler.ctx)
+
+	// Iterate over the cursor and decode todos
+	todos := make([]models.Todo, 0)
+	for cur.Next(handler.ctx) {
+		var todo models.Todo
+		cur.Decode(&todo)
+		todos = append(todos, todo)
+	}
+
+	// Count total documents to calculate total pages
+	totalTodos, err := handler.collection.CountDocuments(handler.ctx, bson.M{})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	totalPages := int(math.Ceil(float64(totalTodos) / float64(pageSize)))
+
+	// Return paginated todos along with total pages and hasMore flag
+	hasMore := page < totalPages
+	responseData := gin.H{
+		"totalPages": totalPages,
+		"data":       todos,
+		"hasMore":    hasMore,
+	}
+	c.JSON(http.StatusOK, responseData)
 }
