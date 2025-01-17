52e10a6cec4380c87228eeb10be5e4899bad0939
Clinton Moshe <clintonmwachia9@gmail.com>
2025-01-17 15:58:23 +0300

    ADDED: config to connect db and create DB indexes

---
+++ config/config.go
@@ -0,0 +1,64 @@
+package config
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var DB *mongo.Client
+
+func LoadEnv() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func ConnectDB() {
+	mongoURL := os.Getenv("MONGO_URL")
+	clientOptions := options.Client().ApplyURI(mongoURL)
+
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		log.Fatal("Error connecting to MongoDB:", err)
+	}
+
+	DB = client
+	log.Println("Connected to MongoDB successfully")
+}
+
+func SetupIndexes() {
+	todoCollection := DB.Database("myapp").Collection("todos")
+	userCollection := DB.Database("myapp").Collection("users")
+
+	// Index for Todos
+	todoIndexes := []mongo.IndexModel{
+		{
+			Keys: bson.D{{Key: "userID", Value: 1}},
+		},
+		{
+			Keys: bson.D{{Key: "createdAt", Value: -1}},
+		},
+	}
+
+	// Index for Users
+	userIndexes := []mongo.IndexModel{
+		{
+			Keys:    bson.D{{Key: "email", Value: 1}},
+			Options: options.Index().SetUnique(true),
+		},
+	}
+
+	if _, err := todoCollection.Indexes().CreateMany(context.Background(), todoIndexes); err != nil {
+		log.Fatal("Error creating todo indexes:", err)
+	}
+	if _, err := userCollection.Indexes().CreateMany(context.Background(), userIndexes); err != nil {
+		log.Fatal("Error creating user indexes:", err)
+	}
+	log.Println("MongoDB indexes created successfully")
+}
--- go.mod
+++ go.mod
@@ -3,9 +3,8 @@ module go-starter-template
 go 1.23.4
 
 require (
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.10.0
-	github.com/golang-jwt/jwt/v5 v5.2.1
+	github.com/joho/godotenv v1.5.1
 	go.mongodb.org/mongo-driver v1.17.2
 )
 
