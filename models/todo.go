8496019147d0a0a74be90ded3cba1baf19e19c38
Clinton Moshe <clintonmwachia9@gmail.com>
2025-01-17 15:14:06 +0300

    ADDED: user and todo models

--- models/todo.go
+++ models/todo.go
@@ -8,11 +8,10 @@ import (
 
 type Todo struct {
 	ID          primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-	UserID      primitive.ObjectID `bson:"user_id" json:"user_id"`
 	Title       string             `bson:"title" json:"title" binding:"required"`
 	Description string             `bson:"description" json:"description"`
 	Completed   bool               `bson:"completed" json:"completed"`
-	DueDate     time.Time          `bson:"due_date" json:"due_date"`
+	UserID      primitive.ObjectID `bson:"user_id" json:"user_id"`
 	CreatedAt   time.Time          `bson:"created_at" json:"created_at"`
 	UpdatedAt   time.Time          `bson:"updated_at" json:"updated_at"`
 }
--- models/user.go
+++ models/user.go
@@ -8,9 +8,9 @@ import (
 
 type User struct {
 	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-	Username  string             `bson:"username" json:"username" binding:"required"`
-	Password  string             `bson:"password" json:"password" binding:"required"`
+	Name      string             `bson:"name" json:"name" binding:"required"`
 	Email     string             `bson:"email" json:"email" binding:"required,email"`
+	Password  string             `bson:"password" json:"password" binding:"required"`
 	Role      string             `bson:"role" json:"role"`
 	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
 	UpdatedAt time.Time          `bson:"updated_at" json:"updated_at"`
